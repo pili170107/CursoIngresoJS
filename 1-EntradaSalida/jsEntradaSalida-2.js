b93c4630863b2349349bf3f4db16601307da0617
pili170107 <eliaschallen@gmail.com>
2017-06-26 12:39:28 -0300

    hola

--- 1-EntradaSalida/jsEntradaSalida-2.js
+++ 1-EntradaSalida/jsEntradaSalida-2.js
@@ -1,7 +1,10 @@
 /*Debemos lograr tomar un nombre con 'prompt' 
 y luego mostrarlo por 'alert' al presionar el botón  'MOSTRAR'*/
-function Mostar()
+function mostrar ()
 {
+    var nombre;
+    nombre=prompt("ingrese datos");
+    alert (nombre);
 	
 }
 
